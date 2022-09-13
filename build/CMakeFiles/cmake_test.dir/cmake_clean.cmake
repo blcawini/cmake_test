02a7f9518ebd42c9c5e7feb458e60098b70204bd
winisforce <winisforce@gmail.com>
2022-09-13 23:35:31 +0900

    ---

---
+++ build/CMakeFiles/cmake_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_test.dir/src/main.cpp.obj"
+  "CMakeFiles/cmake_test.dir/src/main.cpp.obj.d"
+  "cmake_test.exe"
+  "cmake_test.exe.manifest"
+  "cmake_test.pdb"
+  "libcmake_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Works/sforce/code templetes/codes/cmake_test
+# Build directory: D:/Works/sforce/code templetes/codes/cmake_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
